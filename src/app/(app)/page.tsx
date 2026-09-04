ee42995cf6155ebacca109de5175ea7e3ea3024f
Dan Strong <danielstrong@mac.com>
2026-09-04 14:08:19 -0800

    feat: surface Mission Charge on Today

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,13 @@
+import { MissionChargeCard } from "@/components/today/mission-charge-card";
 import { TodayViewV2 } from "@/components/today/today-view-v2";
 
 export default function TodayPage() {
-  return <TodayViewV2 />;
+  return (
+    <>
+      <div className="px-4 pt-4">
+        <MissionChargeCard />
+      </div>
+      <TodayViewV2 />
+    </>
+  );
 }
